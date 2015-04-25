261ec7a93e691e2c1a43e6fcf8fde73fa4ba790e
Manuel Bauer <Manuel.Bauer32@googlemail.com>
2015-04-25 16:53:52 +0200

    Initial commit

---
+++ android/src/main/java/net/manuelbauer/android/ui/BasicRecyclerView.java
@@ -0,0 +1,38 @@
+package net.manuelbauer.android.ui;
+
+import android.content.Context;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by manuel on 25.04.15.
+ */
+public class BasicRecyclerView extends RecyclerView {
+    public BasicRecyclerView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BasicRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    public BasicRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        BasicLinearLayoutManager layoutManager = new BasicLinearLayoutManager(context);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        layoutManager.scrollToPosition(0);
+
+        this.setLayoutManager(layoutManager);
+        this.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL_LIST));
+        this.setItemAnimator(new DefaultItemAnimator());
+        this.setHasFixedSize(true);
+    }
+}
